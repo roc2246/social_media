fe8d7e2ca170209e148f784f69a999ec40578558
Riley <riley.childs@yahoo.com>
2021-11-28 15:44:37 -0500

    Refactored head element into PHP component

---
+++ phpComponents/header.php
@@ -0,0 +1,26 @@
+<?php 
+
+/** 
+ * YourSpace Page Header
+ * 
+ * PHP version 7.4
+ * 
+ * @category PHP
+ * @package  Components
+ * @author   Riley Childs <riley.childs@yahoo.com>
+ * @license  Attribution-ShareAlike 4.0 International (CC BY-SA 4.0)
+ * @link     http://wh963069.ispot.cc/projects/social_media/phpComponents/header.php
+ */
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="#">
+    <link rel="stylesheet" href="style.css">
+    <title>YourSpace - <?php echo $pageName;?></title>
+</head>
