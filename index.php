6520efcd72e2196857fed156d82a69c6b221d331
Riley <riley.childs@yahoo.com>
2021-11-28 15:57:12 -0500

    Developed main UI

--- index.php
+++ index.php
@@ -1,13 +1,41 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Document</title>
-</head>
+<?php 
+
+/** 
+ * YourSpace Main Pge
+ * 
+ * PHP version 7.4
+ * 
+ * @category Pages
+ * @package  Pages
+ * @author   Riley Childs <riley.childs@yahoo.com>
+ * @license  Attribution-ShareAlike 4.0 International (CC BY-SA 4.0)
+ * @link     http://wh963069.ispot.cc/projects/social_media/index.php
+ */
+
+$pageName = 'Homepage';
+ require 'phpComponents/header.php';
+
+?>
 <body>
-    TEST
+    <header>
+        <h1>yourspace</h1>
+        <h4>not <i>my</i> space!</h4>
+    </header>
+
+    <nav>
+        <!--Echo dropdown here for user-->
+        <a href="index.php">home</a> |
+    </nav>
+
+    <main>
+        <h1>Sit tight, the user field will be here soon!</h1>
+        <!--Insert Login Form Here-->
+
+    </main>
+
+    <footer>
+        <p>Copyright 2021</p>
+
+    </footer>
 </body>
 </html>
