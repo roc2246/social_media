7afbbff194317c0b8a12b0d48af7854f43148234
Riley <riley.childs@yahoo.com>
2021-12-17 08:11:35 -0500

    Created UI for "General Info"

--- userHome.php
+++ userHome.php
@@ -26,7 +26,62 @@ require 'phpLibraries/login.php';
 <?php require 'phpComponents/navigation.php';?>
 
     <main>
-
+        <div id="user-profile">
+            <div id="profile-upper-left">
+                <div id="username-and-pic">
+                    <h4>Username</h4>
+                    <img src="" alt="" width="180" height="200">
+                </div>
+                <div id="stats">
+                    <ul style="list-style-type: none;">
+                        <li>Gender</li>
+                        <li>Age</li>
+                        <li>City</li>
+                        <li>State</li>
+                        <li>Country</li>
+                    </ul><br>
+                    <ul style="list-style-type: none;">
+                        <li>Last Login:</li>
+                        <li>Last Login Date</li>
+                    </ul>
+                </div>
+            </div>
+            <p>View My: <a href="">Pics</a> | <a href="">Videos</a></p>
+            <div id="contacting-user">
+                <h4 class="boxHeading">Contacting User</h4>
+                <div id="contacting-user-links">
+                   <div id="contacting-user-links-left"> 
+                       <a href="">
+                           <img src="" alt="">
+                           Send Message
+                        </a>
+                   </div>
+                   <div id="contacting-user-links-right">
+                       <a href="">
+                           <img src="" alt="">
+                           Add to Friends
+                        </a>
+                   </div>
+                </div>
+            </div>
+            <div id="user-url">
+                <b>YourSpace URL:</b>
+                <p style="margin-top:-1px;margin-left:8px;">
+                    User URL
+                </p>
+            </div>
+            <div id="user-general-info">
+                <h4 class="boxHeading">USER: General Info</h4>
+                <p><span class="general-info-heading">Member Since </span> 
+                <span class="general-info-data"> DATE</span></p>
+                <p><span class="general-info-heading">Intersts</span> 
+                 <span class="general-info-data">Empty</span></p>
+                <p><span class="general-info-heading">Activites</span> 
+                 <span class="general-info-data">Empty</span></p>
+                <p><span class="general-info-heading">Other Stuff</span> 
+                <span class="general-info-data"> Empty</span></p>
+            </div>
+        </div>
     </main>
 
 
